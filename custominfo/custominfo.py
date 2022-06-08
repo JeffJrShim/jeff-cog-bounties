3c25a45c0c84dee733088d946eb2269e350575ec
JeffJrShim <71493766+JeffJrShim@users.noreply.github.com>
2022-06-08 21:53:38 +0800

    One commit to shut this down once and for all.

--- custominfo/custominfo.py
+++ custominfo/custominfo.py
@@ -34,26 +34,7 @@ class CustomInfo(commands.Cog):
                 log.info(e)
         self.bot.add_command(info_com)
 
-"""
-<:umiko_love:976859459890577428>**__ABOUT UMIKO__**
-Umiko is a highly advance utility bot and is a fork of red.                          
-The default prefix for the bot is `u!` and `@Umiko`.                                                                                {Image_here}
-Run `@Umiko help` to see all the commands.
-Run `@umiko invite` to invite the bot to your server
-If you have any issues with the bot feel free to contact `! JuliaNGaminG#3569` 
-through his DM's or join the support server [`Support server`](https://discord.gg/A5Ww6HqXmz) or use `@umiko contact <Your_message>`
-<a:pink_dot:971950913923219476>**Instance Owned by:**
-> <a:umiko_crown:976892445579149402>`! JuliaNGaminG#3569 728797399836917771`
-<a:pink_dot:971950913923219476>**Bot's System**
-```
-OS - Windows 11
-Processor - Intel(R) Core(TM) i5-1035G1 CPU @ 1.00GHz   1.19 GHz
-RAM - 8.00 GB
-System type - 64-bit operating system, x64-based processor
-Python version - 3.9.9
-```
-<a:pink_dot:971950913923219476>**Versions**
-> <:umiko_python:977488373834973234>[`3.9.9`](https://www.python.org/) • <:umiko_dpy:977488373834973234>[`1.7.3`](https://github.com/Rapptz/discord.py)
-<a:pink_dot:971950913923219476>**Links**
-> <a:umiko_sip:977488373834973234>[`Invite me`](https://discord.com/api/oauth2/authorize?client_id=962323485772881950&permissions=8&scope=bot) • <a:umiko_dance:977488373834973234>[`Support server`](https://discord.gg/A5Ww6HqXmz)
-"""
+    @commands.command()
+    async def info(self, ctx):
+        embed=discord.Embed(description="<:umiko_love:976859459890577428>**__ABOUT UMIKO__**\nUmiko is a highly advance utility bot and is a fork of red.\nThe default prefix for the bot is `u!` and `@Umiko`.\nRun `@Umiko help` to see all the commands.\nRun `@umiko invite` to invite the bot to your server\nIf you have any issues with the bot feel free to contact `! JuliaNGaminG#3569`\nthrough his DM's or join the support server [`Support server`](https://discord.gg/A5Ww6HqXmz) or use `@umiko contact <Your_message>`\n\n<a:pink_dot:971950913923219476>**Instance Owned by:**> <a:umiko_crown:976892445579149402>`! JuliaNGaminG#3569 728797399836917771` \n\n<a:pink_dot:971950913923219476>**Bot's System** \n```\nOS - Windows 11\nProcessor - Intel(R) Core(TM) i5-1035G1 CPU @ 1.00GHz   1.19 GHz\nRAM - 8.00 GB\nSystem type - 64-bit operating system, x64-based processor\nPython version - 3.9.9\n```\n\n<a:pink_dot:971950913923219476>**Versions**\n> <:umiko_python:977488373834973234>[`3.9.9`](https://www.python.org/) • <:umiko_dpy:977488373834973234>[`1.7.3`](https://github.com/Rapptz/discord.py)\n> <a:umiko_sip:977488373834973234>[`Invite me`](https://discord.com/api/oauth2/authorize?client_id=962323485772881950&permissions=8&scope=bot) • <a:umiko_dance:977488373834973234>[`Support server`](https://discord.gg/A5Ww6HqXmz)")
+        await ctx.send(embed=embed)
